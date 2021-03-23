f60aa3f09d2276feca0ce49eafbbc271d65e169d
ikhsannurhakiki <nurhakikiikhsan@gmail.com>
2021-03-23 08:19:11 +0700

    add circleimageview libarary

--- app/src/main/java/com/example/mylistview/HeroAdapter.kt
+++ app/src/main/java/com/example/mylistview/HeroAdapter.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import de.hdodenhof.circleimageview.CircleImageView
 
 
 data class HeroAdapter internal constructor(private val context: Context) : BaseAdapter() {
@@ -38,7 +39,7 @@ data class HeroAdapter internal constructor(private val context: Context) : Base
     private inner class ViewHolder internal constructor(view: View){
         private val txtName : TextView = view.findViewById(R.id.txt_name)
         private val txtDescription : TextView = view.findViewById(R.id.txt_description)
-        private val imgPhoto : ImageView = view.findViewById(R.id.img_photo)
+        private val imgPhoto : CircleImageView = view.findViewById(R.id.img_photo)
 
         internal fun bind (hero:Hero){
             txtName.text = hero.name
